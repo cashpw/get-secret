9dc5f6fcf605c311c170d267c8479fc27930c334
Cash Prokop-Weaver <cash@cashpw.com>
2024-11-21 10:47:20 -0800

    ✨ get-secret -> secret

--- secret.el
+++ secret.el
@@ -1,4 +1,4 @@
-;;; get-secret.el --- Simple secret management -*- lexical-binding: t; -*-
+;;; secret.el --- Simple secret management -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2024 Cash Prokop-Weaver
 ;;
@@ -7,7 +7,7 @@
 ;; Created: July 02, 2024
 ;; Modified: July 02, 2024
 ;; Version: 0.0.1
-;; Homepage: https://github.com/cashweaver/get-secret
+;; Homepage: https://github.com/cashweaver/secret
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
@@ -19,29 +19,33 @@
 ;;  Example:
 ;;
 ;;    1. Save an API key in ~/foo/my-secret.txt
-;;    2. (setq get-secret--dir "~/foo")
-;;    3. (get-secret "my-secret.txt")
+;;    2. (set-secret-dir "~/foo")
+;;    3. (secret "my-secret.txt")
 ;;
 ;;; Code:
 
-(defgroup get-secret nil
+(defgroup secret nil
   "Simple secret management."
   :group 'emacs)
 
-(defcustom get-secret--dir user-emacs-directory
+(defcustom secret--dir user-emacs-directory
   "Path to directory containing secret files."
-  :group 'get-secret
+  :group 'secret
   :type 'string)
 
-(defun get-secret (name)
+(defun secret-get (name)
   "Get content of NAME secret file."
-  (let ((secret-file-path (format "%s/%s" get-secret--dir name)))
+  (let ((secret-file-path (format "%s/%s" secret--dir name)))
     (if (file-exists-p secret-file-path)
         (string-clean-whitespace
          (with-temp-buffer
            (insert-file-contents secret-file-path)
-           (buffer-string)))
-      "")))
+           (buffer-substring-no-properties (point-min) (point-max))))
+      (error "Secret not found. No file found at %s." secret-file-path))))
 
-(provide 'get-secret)
-;;; get-secret.el ends here
+(defun set-secret-dir (dir)
+  "Set `secret--dir' to DIR."
+  (setq secret--dir dir))
+
+(provide 'secret)
+;;; secret.el ends here
